596ccdd11396c883fed3b57ae1fb053c02f18eca
Florian Thiel <flth@flth.de>
2023-08-24 20:34:09 +0200

    initial von Bandion

---
+++ mod.lua
@@ -0,0 +1,28 @@
+﻿function data()
+return {
+	info = {
+		minorVersion = 0,
+		severityAdd = "NONE",
+		severityRemove = "WARNING",
+		name = _("mod_name"),
+		description = _("mod_desc"),
+		authors = {
+		    {
+		        name = "ModWerkstatt",
+		        role = "CREATOR",
+		    },
+		},
+		tags = { "europe", "waggon", "schuettgut", "deutschland", "germany", "UIC", "gueterwagen", "db" },
+		tfnetId = { },
+		minGameVersion = 0,
+		dependencies = { },
+		url = { "" },
+	},
+	options = {
+	},
+
+	runFn = function (settings)
+
+	end
+	}
+end
---
+++ strings.lua
@@ -0,0 +1,40 @@
+function data()
+return {
+	en = {
+		["mod_name"] = "Viehwagen",
+		["mod_desc"] = "In addition to the mining and steel industry, agriculture was an important customer for the railways. Not only transport capacities for fertilizers and crops were required here, but also the products of livestock farming were largely transported by rail. A special position played in this Frame the transport of small livestock, because a special wagon design was necessary if the loading space was to be used well.Small livestock include, among other things, pigs, goats, sheep and poultry has numerous ventilation openings compared to the normal goods wagons. In order to be able to use the loading space as well as possible, one or three intermediate floors were built into the cattle wagons drawing are the features typical of the federal state railways, e.g. a curved brakeman's cab roof, lower boxes in the middle of the car and bulkhead doors. The cattle truck was built both with and without a handbrake.",
+
+        ["vw_menu"] = "Viehwagen V14",
+        ["vw2_menu"] = "Viehwagen V24",
+		["vh14a_name"] = "V14 without brakes",
+		["vh14b_name"] = "V14 with brake cab",
+		["vh14c_name"] = "V14 modified",
+		["vh14d_name"] = "V14 DRG with brake cab",
+		["vh14e_name"] = "V14 DRG",
+		["vh14f_name"] = "V14 DB",
+		["vh24a_name"] = "V24 DRG",
+		["vh24b_name"] = "V24 DB",
+		
+		
+		["vh14_desc"] = "Cattle car is a colloquial term for a railway wagon used for transporting animals. Livestock was and is usually transported in boxcars or open wagons. Boxcars, which are generally intended for the transport of weather-sensitive goods, were also intended for the transport of animals. Only Stable wagons were used to transport racehorses and crate wagons were used for small livestock (sheep, goats, poultry and rabbits).Cattle but also horses and pigs were transported in open goods wagons (high side wagons), formerly also known as livestock wagons.For the transport of military horses Corresponding tie rings were provided in goods wagons, among other things. The transport of large and small livestock requires special precautions (ventilation flaps, fasteners, watering devices and inspection holes) in order to avoid quantitative and qualitative losses. Military teams were also transported in covered goods wagons.",
+	},
+	de = {
+		["mod_name"] = "Viehwagen",
+		["mod_desc"] = "Neben der Bergbau- und Stahlindustrie war die Landwirtschaft ein bedeutender Auftraggeber für die Eisenbahn. Hier waren nicht nur Transportkapazitäten für die Düngemittel und Feldfrüchte gefragt, auch die Produkte der Viehzucht wurden zu einem großen Teil mit der Bahn befördert. Eine besondere Stellung nahm in diesem Rahmen die Beförderung von Kleinvieh ein, weil dafür eine besondere Wagenbauart notwendig war, wenn der Laderaum gut ausgenutzt werden sollte. Zum Kleinvieh zählen u.a., Schweine, Ziegen, Schafe und Geflügel.Kennzeichnend für die allgemeine Bauart der Verschlagwagen ist der Wagenkasten, der im Gegensatz zu den gewöhnlichen Güterwagen zahlreiche Lüftungsöffnungen besitzt. Um den Laderaum möglichst gut ausnutzen zu können, baute man in die Viehwagen einen oder drei Zwischenböden ein. In der Musterzeichnung A8 des Deutschen Staatsbahnwagenverbandes ist die vereinheitlichte Konstruktion von Viehwagen mit 15t Ladegewicht festgelegt. Im Gegensatz zu dieser Zeichnung sind die länderbahntypischen Merkmale z.b. ein gewölbtes Bremserhausdach, Unterkästen in Wagenmitte und Stirnwandtüren. Der Viehwagen wurde sowohl mit als auch ohne Handbremse gebaut.",
+
+        ["vw_menu"] = "Viehwagen V14",
+        ["vw2_menu"] = "Viehwagen V24",
+		["vh14a_name"] = "V14 ungebremst",
+		["vh14b_name"] = "V14 mit Bremserhaus",
+		["vh14c_name"] = "V14 modernisiert",
+		["vh14d_name"] = "V14 DRG mit Bremserhaus",
+		["vh14e_name"] = "V14 DRG",
+		["vh14f_name"] = "V14 DB",
+		["vh24a_name"] = "V24 DRG",
+		["vh24b_name"] = "V24 DB",
+		
+		
+		["vh14_desc"] = "Viehwaggon ist ein umgangssprachlicher Begriff für einen Eisenbahnwagen zum Tiertransport. Vieh wurde und wird in der Regel in gedeckten oder offenen Güterwagen befördert. Gedeckte Güterwagen, die im allgemein für den Transport von witterungsempfindlichen Gütern bestimmt sind, waren aber auch für den Tiertransport vorgesehen. Nur für den Transport von Rennpferden wurden Stallungswagen verwendet und für Kleinvieh (Schafe, Ziegen, Geflügel und Kaninchen) wurden Verschlagwagen verwendet. In offenen Güterwagen (Hochbordwagen), früher auch Hornviehwagen genannt, wurden Rinder aber auch Pferde oder Schweine transportiert. Für die Beförderung von Militärpferden in Güterwagen wurden unter anderem entsprechende Anbinde-Ringe vorgesehen. Der Transport von Groß- und Kleinvieh verlangt besondere Vorkehrungen (Lüftungsklappen, Befestigungsmittel, Tränkvorrichtungen und Schaulöcher), um quantitative und qualitative Verluste zu vermeiden. Auch Militärmannschaften wurden in gedeckten Güterwagen befördert.",
+	},
+}
+end
